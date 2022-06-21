aa5a60d467d273ac80b8ba9f212effe92b5f3c8c
P Balaramakrishna Varma <balaramakrishna.p@students.iiit.ac.in>
2022-06-21 19:40:21 +0530

    Testing 2/5 done

--- Test/test_hash_function.cpp
+++ Test/test_hash_function.cpp
@@ -2,13 +2,20 @@
 #include "../src/Common/hash_interface.h"
 #include "stdio.h"
 #include "string.h"
+#include "gtest/gtest.h"
 
-int main()
+TEST(Hash_function, Hash_geek)
 {
     char A[] = "abcdefgh";
     int N = strlen(A);
     unsigned int Hash_geek = string_hash_geek(A, N);
+    EXPECT_EQ(Hash_geek, 489951694);
+}
+
+TEST(Hash_function, Hash_Murmur3)
+{
+    char A[] = "abcdefgh";
+    int N = strlen(A);
     unsigned int Hash_Murmur3 = string_hash_murmur3(A, N);
-    printf("Hash_geek: %ld\n", Hash_geek);
-    printf("Hash_Murmur3: %ld\n", Hash_Murmur3);
+    EXPECT_EQ(Hash_Murmur3, 3348134347);
 }
