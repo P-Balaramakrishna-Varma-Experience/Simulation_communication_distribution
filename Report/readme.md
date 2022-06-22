a2da811dfd5479efbf7126b6032d04dd4178b875
P Balaramakrishna Varma <balaramakrishna.p@students.iiit.ac.in>
2022-06-22 11:39:26 +0530

    Some small changes

---
+++ Report/readme.md
@@ -0,0 +1,7 @@
+- input experiment name + all files in 
+
+## files which contain data are in csv format. (csv libaray)
+
+
+
+
--- Runner.sh
+++ Runner.sh
@@ -2,19 +2,20 @@
 Dir=$1
 Exe_Name=$2
 FileName=$3
+Version=$4
 #echo $Dir $Exe_Name $File_Path
 
 
 #Initalizing file
 mkdir ./Data/$Dir
-rm ./Data/$Dir/$Exe_Name_$FileName.csv
-touch ./Data/$Dir/$Exe_Name_$FileName.csv
-echo "k,cost" > ./Data/$Dir/$Exe_Name$FileName.csv
+rm ./Data/$Dir/$Exe_Name$FileName$Version.csv
+touch ./Data/$Dir/$Exe_Name$FileName$Version.csv
+echo "k,cost" > ./Data/$Dir/$Exe_Name$FileName$Version.csv
 
 
 #Filling data to the file
 K_values=("10" "30" "50" "100" "200")
 for K in ${K_values[@]}; do 
     #echo $K
-    ./Executables/$Exe_Name ./Input_DNA/$FileName $K >> ./Data/$Dir/$Exe_Name$FileName.csv
+    ./Executables/$Exe_Name ./Input_DNA/$FileName $K >> ./Data/$Dir/$Exe_Name$FileName$Version.csv
 done
