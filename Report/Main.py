0d2a63c3e2e01e4cb08ea6165789a28c31c9b552
P Balaramakrishna Varma <balaramakrishna.p@students.iiit.ac.in>
2022-06-24 09:14:24 +0530

    Reporting done

--- Report/Main.py
+++ Report/Main.py
@@ -17,43 +17,64 @@ def readcsv(cur_dir, filename, col_no, Dir):
         return Ans
 
 
-# type == comm or dist
-def PlotAll(Dir, Dirs, type):
-    assert(type == 'comm' or type == 'dist')
-    for dir_name in Dirs:
-        Temp_all_files = os.listdir("./Data/" + Dir +  "/" + dir_name)
-        Temp_all_files_csv = [f for f in Temp_all_files if f.endswith(".csv")]
-        files = [f for f in Temp_all_files_csv if type in f]
-
-        Data = []
-        for file in files:
-            Data.append(readcsv(dir_name, file, 2, Dir))
-
-        for index in range(len(Data)):
-            plt.plot(K_Values, Data[index], label=extract_version(files[index]))
-
-        plt.legend()
-        plt.xlabel("K values")
-        if type == "comm":
-            plt.ylabel("Communication costs")
-            plt.savefig("./Plots/" + dir_name + "Comm_costs_" + dir_name + ".png")
-        else:
-            plt.ylabel("Variance of distribution")
-            plt.savefig("./Plots/" + dir_name + "dist_var" + dir_name + ".png")
-        plt.clf()
-
-
-
-
-
-
-# Get the directory name
 assert(len(sys.argv) == 2)
 Dir = sys.argv[1]
 Dirs = os.listdir("./Data/" + Dir)
-K_Values = [1, 3, 5, 10]
-
-PlotAll(Dir, Dirs, 'comm')
-#PlotAll(Dir, Dirs, 'dist')
+K_Values = ["1", "3", "5", "10"]
+Hash_Functions = ['geeks', 'murmur3']
 
 
+#ploting communication costs
+for dir_name in Dirs:
+    Temp_all_files = os.listdir("./Data/" + Dir +  "/" + dir_name)
+    Temp_all_files_csv = [f for f in Temp_all_files if f.endswith(".csv")]
+    files = [f for f in Temp_all_files_csv if "comm" in f]
+
+    Data = []
+    for file in files:
+        Data.append(readcsv(dir_name, file, 2, Dir))
+
+    for index in range(len(Data)):
+        plt.plot(K_Values, Data[index], label=extract_version(files[index]))
+
+    plt.legend()
+    plt.xlabel("K values")
+    plt.ylabel("Communication costs")
+    plt.savefig("./Plots/" + Dir + "Comm_costs_" + dir_name + ".png")
+    plt.clf()
+
+
+#plotting distribution
+# number of graphs = #k * #hash * #Files
+# in each plot all exe results are plotted
+for dir_name in Dirs:
+    Temp_all_files = os.listdir("./Data/" + Dir +  "/" + dir_name)
+    Temp_all_files_csv = [f for f in Temp_all_files if f.endswith(".csv")]
+    files = [f for f in Temp_all_files_csv if "dist" in f]
+
+    Data = []
+    for file_name in files:
+        Dict = {}
+        File_Path = "./Data/" + Dir + "/" + dir_name + "/" + file_name
+        with open(File_Path, 'r') as f:
+            reader = csv.reader(f)
+            for row in reader:
+                if (row[0], row[2]) not in Dict:
+                    Dict[(row[0], row[2])] = {}
+                Dict[(row[0], row[2])][row[1]] = row[3]
+        Data.append(Dict)
+
+
+    for hash_func in Hash_Functions:
+        for K in K_Values:
+            for index in range(len(files)):
+                X_Strings = Data[index][(K, hash_func)].keys()
+                Y_Strings = Data[index][(K, hash_func)].values()
+                X_values = [int(x) for x in X_Strings]
+                Y_values = [float(y) for y in Y_Strings]
+                plt.plot(X_values, Y_values, label=extract_version(files[index]))
+                plt.legend()
+                plt.xlabel("T values")
+                plt.ylabel("variance")
+                plt.savefig("./Plots/" + Dir + "Dist_" + dir_name + hash_func + "_" + str(K) + "_"  + ".png")
+            plt.clf()
