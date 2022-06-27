0a47ccd5c1ed600b02fef2f727db180ade7f1b1c
P Balaramakrishna Varma <balaramakrishna.p@students.iiit.ac.in>
2022-06-27 15:23:26 +0530

    super-mer reader checked

--- src/Common/ClassicalMinimizer.h
+++ src/Common/ClassicalMinimizer.h
@@ -3,8 +3,15 @@
 
 #include "stdbool.h"
 
+//kmer, K input kmers is passed (make sure valid)
+//Ans, M is passed to store output
 void Classic_Minimizer(char* Ans, int Mlength, int K, char* kmer);
+
+//kmer, K input kmers is passed (make sure valid)
+//returns the index of start of the minimizer
 int Classic_Minimizer_loc(int Mlenght, int K, char* kmer);
+
+// < 0 if str1 < str2  => str1 better than str2
 int better_classic_mini(char* str1, char* str2, int M);
 
 #endif
--- src/Common/supermer_reader.h
+++ src/Common/supermer_reader.h
@@ -3,6 +3,12 @@
 
 #include "stdio.h"
 
+// Storage is char array of len sN
+// Min is char array of len M
+// K is according to the input
+// return lenght of the supermer which starts at storage
+// return -1 means file reading done do no more.
+//use only once function since uses static variables initalization.
 int get_next_supermer(FILE* file, int K, int M, char *Min, char* Storage, int sN);
 
 #endif
