9601980f88ab0d29524a651f942b20580b52cbf7
P Balaramakrishna Varma <balaramakrishna.p@students.iiit.ac.in>
2022-07-03 13:05:21 +0530

    Binning effectevily will solve the problem.. of distribution => communication benifits with not down side.  + number of times hash function being called reduces

--- minizier_binning.py
+++ minizier_binning.py
@@ -58,6 +58,25 @@ def plot_dist(record, Threads):
     plt.ylabel("Variance")
     plt.savefig("testing.png")
 
+def give_var(record, Threads):
+    x_values = Threads
+    y_values = []
+    for no_threads in Threads:
+        Map = genmap_rand(16384, no_threads)
+        Distribution = convert_record(16384, no_threads, Map, record)
+        y_value = Measure(Distribution)
+        y_values.append(y_value)
+    return y_values
+
+def Fist_smaller(A, B):
+    assert(len(A) == len(B))
+    sum = 0
+    for i in range(len(A)):
+        sum = sum + (A[i] - B[i]) 
+    if(sum < 0):
+        return True
+    else:
+        return False
 
 Threads = [4, 8, 16, 32, 64, 128, 256, 512, 1024, 2048, 4096, 8192]
 
@@ -65,6 +84,22 @@ data_json = open('data.json')
 Data = json.load(data_json)
 record = Data[0]['record'][0]['Data']
 
-N = 10
+N = 10000
+
+min = [111111 for _ in range(len(Threads))]
+max = [0 for _ in range(len(Threads))]
 for _ in range(N):
-    plot_dist(record, Threads)
+    cur = give_var(record, Threads)
+    if(Fist_smaller(cur, min)):
+        min = cur
+    if(Fist_smaller(max, cur)):
+        max = cur
+
+plt.xscale('log')
+plt.plot(Threads, max, marker = 'o')
+plt.plot(Threads, min, marker = 'o')
+plt.xlabel("No of threads")
+plt.ylabel("Variance")
+plt.savefig("testing.png")
+print(min)
+print(max)
